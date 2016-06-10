ecfd64ef513fb2f552e5cd9a10c2f126cc183786
Harish Ved <ved.harish3@gmail.com>
2016-06-10 17:24:51 +0530

    Added Local Tests

---
+++ Local Test/Local Test/LocalStatus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+namespace SpecFlow.BrowserStack.LocalTests
+{
+  [Binding]
+  public class LocalStatus
+  {
+    readonly IWebDriver _driver;
+
+    public LocalStatus()
+    {
+      _driver = (IWebDriver)ScenarioContext.Current["driver"];
+    }
+
+    [Given(@"I am on the google page")]
+    public void GivenIAmOnTheGooglePage()
+    {
+      _driver.Navigate().GoToUrl("http://www.google.com");
+    }
+
+    [When(@"I navigate to (.*)")]
+    public void WhenINavigateTo(String navigateUrl)
+    {
+      _driver.Navigate().GoToUrl(navigateUrl);
+    }
+
+    [Then(@"I see status as (.*)")]
+    public void ThenISeeStatusAs(String expectedStatus)
+    {
+      StringAssert.Contains(_driver.FindElement(By.TagName("pre")).Text.Trim().ToLower(), "up and running");
+    }
+
+  }
+}
---
+++ Local Test/Local Test/LocalStatus.feature
@@ -0,0 +1,6 @@
+﻿Feature: Local Connected Status
+
+Scenario: Checking Local connection Status
+Given I am on the google page
+When I navigate to http://localhost:45691/check
+Then I see status as up and running
